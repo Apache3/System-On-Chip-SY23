3f8ae28f44996269f7d585d09dedf340505fee6c
Apache3 <tj.thomas.jeantet@gmail.com>
2017-11-08 11:39:03 +0100

    cleaned code

--- src/components/PWM/PWM.vhd
+++ src/components/PWM/PWM.vhd
@@ -1,82 +1,46 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    12:42:51 10/20/2017 
--- Design Name: 
--- Module Name:    PWM - PWM_arch 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use ieee.numeric_std.all;
 use work.all;
 use ieee.std_logic_unsigned.all;
 
--- Uncomment the following library declaration if using
---arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity PWM is
-Port( clk: in STD_LOGIC;
-		rst: in STD_LOGIC;
-		clk_div: in STD_LOGIC;
-		duty_cycle: in STD_LOGIC_VECTOR(7 downto 0);
-		pwm_out: out STD_LOGIC );		 
+Port( 	clk 		: in  STD_LOGIC;
+		rst			: in  STD_LOGIC;
+		clk_div		: in  STD_LOGIC;
+		duty_cycle	: in  STD_LOGIC_VECTOR(7 downto 0);
+		pwm_out		: out STD_LOGIC );		 
 
 end PWM;
 
 architecture PWM_arch of PWM is
 
-
-
-
 signal cpt : STD_LOGIC_VECTOR(7 downto 0);
 
 begin
 
-
-
-pwm: process(clk, clk_div,cpt, rst, duty_cycle)
-begin
-if rst = '1' then
-	cpt <= (others =>'0');
-	pwm_out <='0';
-else
-	if rising_edge(clk) and clk_div = '1' then
-	cpt <= cpt + 1;
+	pwm: process(clk, clk_div,cpt, rst, duty_cycle)
+	begin
+		if rst = '1' then
+			cpt <= (others =>'0');
+			pwm_out <='0';
+		else
+			if rising_edge(clk) and clk_div = '1' then
 	
-	end if;
+				cpt <= cpt + 1;
+			
+			end if;
 
-	if (cpt > duty_cycle ) then
-		pwm_out <= '0';
-	else 
-		pwm_out <='1';
-	end if;
-	
-end if;
- 
- 
+			if (cpt > duty_cycle ) then
+				pwm_out <= '0';
+			else 
+				pwm_out <='1';
+			end if;
+			
+		end if;
 
-
-end process pwm;
-
-
+	end process pwm;
 
 end PWM_arch;
 
--- src/components/PWM/PWM_with_divider_tb.vhd
+++ src/components/PWM/PWM_with_divider_tb.vhd
@@ -1,36 +1,6 @@
---------------------------------------------------------------------------------
--- Company: 
--- Engineer:
---
--- Create Date:   10:53:37 10/27/2017
--- Design Name:   
--- Module Name:   /home/uvs/xilinx/Apache Babar/PWM/PWM_with_divider_tb.vhd
--- Project Name:  PWM
--- Target Device:  
--- Tool versions:  
--- Description:   
--- 
--- VHDL Test Bench Created by ISE for module: PWM
--- 
--- Dependencies:
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
---
--- Notes: 
--- This testbench has been automatically generated using types std_logic and
--- std_logic_vector for the ports of the unit under test.  Xilinx recommends
--- that these types always be used for the top-level I/O of a design in order
--- to guarantee that the testbench will bind correctly to the post-implementation 
--- simulation model.
---------------------------------------------------------------------------------
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
- 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---USE ieee.numeric_std.ALL;
+
  
 ENTITY PWM_with_divider_tb IS
 END PWM_with_divider_tb;
