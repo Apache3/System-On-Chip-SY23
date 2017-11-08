0037bc43d13674965cf3248ae4a62920b76d4e0e
Apache3 <tj.thomas.jeantet@gmail.com>
2017-11-08 23:00:43 +0100

    added timer components, and comments on registers

--- src/soc/avrtiny861/timer.vhd
+++ src/soc/avrtiny861/timer.vhd
@@ -38,18 +38,61 @@ architecture timer_architecture of timer is
 
 
 
-constant OCR1A  : integer := BASE_ADDR ;
-constant TCNT1  : integer := BASE_ADDR + 1;
-constant TCCR1B : integer := BASE_ADDR + 2;
-constant TCCR1A : integer := BASE_ADDR + 3;
+constant OCR1A  : integer := BASE_ADDR ; --registre de comparaison
+constant TCNT1  : integer := BASE_ADDR + 1; --valeur du compteur
+constant TCCR1B : integer := BASE_ADDR + 2; --registre controle B
+constant TCCR1A : integer := BASE_ADDR + 3; --registre controle A
 
 signal reg_compA : STD_LOGIC_VECTOR (7 downto 0);
-signal reg_count : STD_LOGIC_VECTOR (7 downto 0);
-signal reg_ctrlA : STD_LOGIC_VECTOR (7 downto 0);
+-- b7:
+-- b6:
+-- b5:
+-- b4:
+-- b3:
+-- b2:
+-- b1:
+-- b0:
+
+signal reg_count : STD_LOGIC_VECTOR (7 downto 0); 
+-- b7:
+-- b6:
+-- b5:
+-- b4:
+-- b3:
+-- b2:
+-- b1:
+-- b0:
+
+signal reg_ctrlA : STD_LOGIC_VECTOR (7 downto 0); 
+-- b7,b6: mod fct comp A :
+  --00 Z sur sorite 
+  --01 0 à comparaison , 1 si registre compteur à 0 (oc et ocbar)
+  --10 0 à comparaison, 1 si registre compteur à 0 (oc
+  --11 1 à comparaison , 1 si registre compteur à 1 (oc et ocbar)
+-- 
+-- b5,b4: mod fct comp B :comme b7 et b6 mais osef
+-- b3: force sortie A 
+-- b2:force sortie B 
+-- b1:pwm A 
+-- b0:pwm B
+
+
 signal reg_ctrlB : STD_LOGIC_VECTOR (7 downto 0);
+-- b7:
+-- b6:
+-- b5:
+-- b4:
+-- b3:
+-- b2:
+-- b1:
+-- b0:
 
-signal gen_div_out, pow_div_out : std_logic;
-
+signal gen_div_out    : std_logic; --sortie diviseur
+signal pow_div_out    : std_logic; --sortie diviseur de puissance
+signal pwm_clk        : std_logic; --entrée timer du pwm
+signal pwm_out        : std_logic; --sortie pwm
+signal div_on_bar     : std_logic; --signal inverse de l'activation du diviseur
+signal pow_div_on_bar : std_logic; --signal inverse de l'activation du diviseur de puissance
 
 
 begin
@@ -69,6 +112,17 @@ begin
       pow_div => reg_ctrlB(5 downto 4),
       clk_out => pow_div_out
       );
+
+  pwm_comp : pwm 
+    port map(
+      clk => clk,
+      rst => rst,
+      clk_div => pwm_clk,
+      
+      pwm_out => pwm_out;
+      );
+
+
 
 
 	
